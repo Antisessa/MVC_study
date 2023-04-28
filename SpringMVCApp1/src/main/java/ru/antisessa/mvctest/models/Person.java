27562b55a6ace9448fc0a38bce574c81b45abfa8
Antisessa <118179929+Antisessa@users.noreply.github.com>
2023-04-28 15:26:48 +0300

    from local

---
+++ SpringMVCApp1/src/main/java/ru/antisessa/mvctest/models/Person.java
@@ -0,0 +1,14 @@
+package ru.antisessa.mvctest.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Person {
+    private int id;
+    private String name;
+
+}
---
+++ SpringMVCApp1/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+<a href="/people">Go to People list</a>
+</html>
