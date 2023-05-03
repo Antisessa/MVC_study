0da014b83d56f5304da61bd595a0a0cd7e87e6b6
Antisessa <118179929+Antisessa@users.noreply.github.com>
2023-05-03 14:57:03 +0300

    create save method in PeopleController

--- SpringMVCApp1/src/main/java/ru/antisessa/mvctest/models/Person.java
+++ SpringMVCApp1/src/main/java/ru/antisessa/mvctest/models/Person.java
@@ -2,13 +2,14 @@ package ru.antisessa.mvctest.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class Person {
     private int id;
     private String name;
-
 }
