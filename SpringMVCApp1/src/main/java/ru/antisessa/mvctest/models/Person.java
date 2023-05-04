8f108abd418a137d2b71e8e0c0415b84eac54e47
Antisessa <118179929+Antisessa@users.noreply.github.com>
2023-05-04 17:14:17 +0300

    add hibernate validator for edit and new form

--- SpringMVCApp1/src/main/java/ru/antisessa/mvctest/models/Person.java
+++ SpringMVCApp1/src/main/java/ru/antisessa/mvctest/models/Person.java
@@ -5,13 +5,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
+
+    @Min(value = 0, message = "Age should be positive")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
 }
