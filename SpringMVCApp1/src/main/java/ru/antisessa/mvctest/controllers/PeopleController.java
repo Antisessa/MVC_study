03d9581d6cfea5afaf3989c6b0e5c90372317065
Antisessa <118179929+Antisessa@users.noreply.github.com>
2023-05-04 17:14:17 +0300

    add hibernate validator for edit and new form

--- SpringMVCApp1/src/main/java/ru/antisessa/mvctest/controllers/PeopleController.java
+++ SpringMVCApp1/src/main/java/ru/antisessa/mvctest/controllers/PeopleController.java
@@ -3,9 +3,12 @@ package ru.antisessa.mvctest.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.antisessa.mvctest.dao.PersonDAO;
 import ru.antisessa.mvctest.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -33,12 +36,15 @@ public class PeopleController {
 
     @GetMapping("/new")
     public String newPerson(@ModelAttribute("person") Person person) {
-
         return "people/new";
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
+        
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -50,8 +56,12 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person,
-                         @PathVariable("id") int id){
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult, @PathVariable("id") int id){
+
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
